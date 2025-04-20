1e965e956f4b6e20671e57d873a689e09f904198
gironabi <yohannaagiron@gmail.com>
2025-04-19 23:08:37 -0400

    Add files via upload

---
+++ binarysearch.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+int binarySearch(const T list[], T key, int listSize) {
+    int low = 0;
+    int high = listSize - 1;
+
+    while (high >= low) {
+        int mid = (low + high) / 2;
+
+        if (key < list[mid])
+            high = mid - 1;
+        else if (key == list[mid])
+            return mid;
+        else
+            low = mid + 1;
+    }
+
+    return -low - 1;
+}
+
+int main() {
+    int intArray[] = {1, 3, 5, 7, 9};
+    cout << "Index of 5 in intArray: " << binarySearch(intArray, 5, 5) << endl;
+
+    double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    cout << "Index of 4.4 in doubleArray: " << binarySearch(doubleArray, 4.4, 5) << endl;
+
+    string stringArray[] = {"apple", "banana", "cherry", "date", "fig"};
+    cout << "Index of \"cherry\" in stringArray: " << binarySearch(stringArray, string("cherry"), 5) << endl;
+
+    return 0;
+}
